ecdf129c768b71db09c2a604bb3954e4ad54b9c7
iamnezuko <iamnezuko69@gmail.com>
2025-09-14 12:24:05 +0530

    Update app.py

--- Proposal Website/app.py
+++ Proposal Website/app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template
-from datetime import datetime
 
 app = Flask(__name__)
 
@@ -9,14 +8,10 @@ def home():
 
 @app.route("/yes")
 def yes():
-    with open("response.txt", "a") as f:
-        f.write(f"YES at {datetime.now()}\n")
     return render_template("yes.html")
 
 @app.route("/no")
 def no():
-    with open("response.txt", "a") as f:
-        f.write(f"NO at {datetime.now()}\n")
     return render_template("no.html")
 
 if __name__ == "__main__":
